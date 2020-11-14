d208c572eb1c6d53c13f129dd63b8b65080f1b23
Bentleyj <jgbentley10@gmail.com>
2020-11-14 06:45:54 -0500

    Point startPlayer script at screensaver folder

--- startPlayer.bat
+++ startPlayer.bat
@@ -1,3 +1,3 @@
 cd bin
-Viewer.exe --data-path "C:\Users\Hellicar Studio\Documents\SK\data\recordings\2020-08-09-04-45-01" --window-width 1920 --window-height 1080 --window-x 0 --window-y 0
+Viewer.exe --data-path "C:\Users\Hellicar Studio\Documents\SK\data\screensaver" --window-width 1920 --window-height 1080 --window-x 0 --window-y 0
 cd ..
