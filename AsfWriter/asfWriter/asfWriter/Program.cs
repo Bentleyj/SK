c46333a42f67b122d28106b7bd992f080bff50f7
Bentleyj <jgbentley10@gmail.com>
2020-11-14 09:26:02 -0500

    ASFWriter: make program robust against failures.

--- AsfWriter/asfWriter/asfWriter/Program.cs
+++ AsfWriter/asfWriter/asfWriter/Program.cs
@@ -12,6 +12,72 @@ namespace asfWriter
         private static string srcPath = "";
         private static string dstPath = "";
 
+        static void writeFiles(ulong sectionStartTime, int suffix)
+        {
+            AxisFileWriter writer = new AxisFileWriter();
+            ulong lastSuccessfulStartTime = sectionStartTime;
+            try
+            {
+                // Open bin file
+                using (FileStream inFileStream = new FileStream(srcPath, FileMode.Open))
+                using (BinaryReader inFile = new BinaryReader(inFileStream, Encoding.UTF32))
+                {
+                    int mediaTypeSize = inFile.ReadInt32();
+                    byte[] mediaTypeBuffer = inFile.ReadBytes(mediaTypeSize);
+
+                    writer.BeginRecord(dstPath + "_" + suffix.ToString() + ".asf", mediaTypeBuffer);
+
+                    while (inFile.PeekChar() != -1)
+                    {
+                        int sampleType = inFile.ReadInt32();
+                        int sampleFlags = inFile.ReadInt32();
+                        ulong startTime = inFile.ReadUInt64();
+                        ulong stopTime = inFile.ReadUInt64();
+                        int bufferSize = inFile.ReadInt32();
+                        byte[] bufferBytes = inFile.ReadBytes(bufferSize);
+                        if (startTime < sectionStartTime)
+                            continue;
+
+                        Console.WriteLine("type {0}, flages {1}, start {2}, size {3}", sampleType, sampleFlags, startTime, bufferSize);
+                        AFW_STREAM_TYPE streamType;
+                        if (sampleType == (int)AMP_VIDEO_SAMPLE_TYPE.AMP_VST_MPEG4_AUDIO)
+                        {
+                            streamType = AFW_STREAM_TYPE.AFW_ST_AUDIO;
+                        }
+                        else
+                        {
+                            streamType = AFW_STREAM_TYPE.AFW_ST_VIDEO;
+                        }
+                        writer.WriteStreamSample((ushort)streamType, startTime, 0, bufferBytes);
+                        lastSuccessfulStartTime = startTime;
+                    }
+
+                    writer.EndRecording();
+                    Console.WriteLine("Done");
+                    Marshal.FinalReleaseComObject(writer);
+                    writer = null;
+                    Console.ReadLine();
+                }
+            }
+            catch (COMException e)
+            {
+                Console.WriteLine("Error Writing Stream {0}. Attempting to start new writer...", e.Message);
+                try
+                {
+                    writer.EndRecording();
+                    Marshal.FinalReleaseComObject(writer);
+                    writer = null;
+                }
+                catch (COMException e2)
+                {
+                    Console.WriteLine("Error Creating New Stream {0}. Exiting.", e.Message);
+                }
+
+                writeFiles( lastSuccessfulStartTime, ++suffix);
+
+            }
+        }
+
         static void Main(string[] args)
         {
 
@@ -40,51 +106,7 @@ namespace asfWriter
                     Console.WriteLine("Malformed Command: " + args[i] + ",\n Arguments must be preceded by -- and succeeded by their argument. ignoring this argument...");
                 }
             }
-
-            AxisFileWriter writer = new AxisFileWriter();
-            try
-            {
-                // Open bin file
-                using (FileStream inFileStream = new FileStream(srcPath, FileMode.Open))
-                using (BinaryReader inFile = new BinaryReader(inFileStream, Encoding.UTF32))
-                {
-                    int mediaTypeSize = inFile.ReadInt32();
-                    byte[] mediaTypeBuffer = inFile.ReadBytes(mediaTypeSize);
-
-                    writer.BeginRecord(dstPath, mediaTypeBuffer);
-
-                    while (inFile.PeekChar() != -1)
-                    {
-                        int sampleType = inFile.ReadInt32();
-                        int sampleFlags = inFile.ReadInt32();
-                        ulong startTime = inFile.ReadUInt64();
-                        ulong stopTime = inFile.ReadUInt64();
-                        int bufferSize = inFile.ReadInt32();
-                        byte[] bufferBytes = inFile.ReadBytes(bufferSize);
-                        Console.WriteLine("type {0}, flages {1}, start {2}, size {3}", sampleType, sampleFlags, startTime, bufferSize);
-                        AFW_STREAM_TYPE streamType;
-                        if (sampleType == (int)AMP_VIDEO_SAMPLE_TYPE.AMP_VST_MPEG4_AUDIO)
-                        {
-                            streamType = AFW_STREAM_TYPE.AFW_ST_AUDIO;
-                        }
-                        else
-                        {
-                            streamType = AFW_STREAM_TYPE.AFW_ST_VIDEO;
-                        }
-                        writer.WriteStreamSample((ushort)streamType, startTime, 0, bufferBytes);
-                    }
-
-                    writer.EndRecording();
-                    Console.WriteLine("Done");
-                    Marshal.FinalReleaseComObject(writer);
-                    writer = null;
-                    Console.ReadLine();
-                }
-            }
-            catch (COMException e)
-            {
-                Console.WriteLine("Exception {0}", e.Message);
-            }
+            writeFiles( 0, 0);
         }
     }
 }
